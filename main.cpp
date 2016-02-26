8eab3075639c9137b60bcd3fdda4ba2557c5bbe9
sandraakupiyan <sandra0696@gmail.com>
2016-02-26 13:14:23 +0300

    dobavili gitignore

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int a, b;
+int main()
+{
+    printf("vvedi chislo a");
+    scanf("%d",&a);
+    printf("Vvedi chislo b");
+    scanf("%d",&b);
+    printf("%d, %d",a,b);
+    return 0;
+}
