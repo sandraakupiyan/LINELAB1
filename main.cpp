590ac8cc0ddcce5596e2f7a83bd7c37f472315bd
sandraakupiyan <sandra0696@gmail.com>
2016-03-03 13:59:08 +0300

    sum and minus and umnoz

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int a, b, c;
+
 int main()
 {
-    printf("chislo a: ");
-    scanf("%d",&a);
-    printf("chislo b: ");
-    scanf("%d",&b);
-    c=a+b;
-    printf("%d+%d=%d",a,b,c);
-    return 0;
+ int a, b;
+ printf ("vveditr a end b: ");
+ scanf ("%d,%d",&a,&b);
+  printf ("%d+%d=%d",a,b,a+b);
+  printf ("%d-%d=%d",a,b,a-b);
+  printf ("%d*%d=%d",a,b,a*b);
+ return 0;
 }
