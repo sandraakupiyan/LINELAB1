81deeb66ab0cb08599ad291e4c991e9568576c59
sandraakupiyan <sandra0696@gmail.com>
2016-03-09 13:29:27 +0300

    ispravlenie

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
+//This program is just an exampie one under VCS.
+float a,b;
 int main()
 {
     printf("chislo a: ");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("chislo b: ");
-    scanf("%d",&b);
-    printf("%d+%d=%d",a,b,a+b);
-    printf("%d-%d=%d",a,b,a-b);
-    printf("%d*%d=%d",a,b,a*b);
-    printf("%d/%d=%d",a,b,a/b);
+    scanf("%f",&b);
+    printf("%1.0f+%1.0f=%1.0f \n",a,b,a+b);
+    printf("%1.0f-%1.0f=%1.0f \n",a,b,a-b);
+    printf("%1.0f*%1.0f=%1.0f \n",a,b,a*b);
+    printf("%1.0f/%1.0f=%1.4f \n",a,b,a/b);
     return 0;
 }
